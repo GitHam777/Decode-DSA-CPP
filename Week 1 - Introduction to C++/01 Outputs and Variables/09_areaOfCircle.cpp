c4ee9e33063c405937fdd6d446fe5ee4c94fe1ba
Anurag Bhattacharjee <133899812+GitHam777@users.noreply.github.com>
2024-05-02 21:55:14 +0530

    Deleted Files

--- Week 1 - Introduction to C++/01 Outputs and Variables/09_areaOfCircle.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(void)
-{
-    float radius = 10;
-    float area;
-    area = 3.1415 * radius * radius;
-    cout << "Area of circle: " << area << endl;
-}
--- Week 1 - Introduction to C++/01 Outputs and Variables/11_output2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(void)
-{
-    int a = 0, b = 5;
-    // cout << b << endl;
-    cout << a << endl;
-}
